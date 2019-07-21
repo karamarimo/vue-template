f2c7496fa3c64eb1fb0f1ac55e9aff45dfaf9fb2
karamarimo <16463516+karamarimo@users.noreply.github.com>
2019-07-22 04:29:35 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="hideen_area">aaaa</div>
+</template>
+
+<script>
+const created = function() {
+  setTimeout(() => {
+    console.log("hidden_area_show()呼び出します")
+    this.hidden_area_show()
+  }, 3000)
+}
+
+const methods = {
+  hidden_area_show: function() {
+    console.log("hidden_area表示します")
+    document.getElementById("hideen_area").style.display = "block"
+    console.log("hidden_area表示しました")
+  }
+}
+
+export default {
+  created,
+  methods
+}
+</script>
+
+<style>
+#hideen_area {
+  display: none;
+}
+</style>
